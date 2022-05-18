955fdc8baf2b5d03a06a88f3ee7fc52c0d5e40cb
dzornyj <101612220+dzornyj@users.noreply.github.com>
2022-05-18 00:01:54 -0500

    questions_started_answering

--- codesolution.sql
+++ codesolution.sql
@@ -1,17 +1,110 @@
 
+CREATE TABLE departments (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+--verify table departments was created successfully
+SELECT * FROM departments
+
+
+CREATE TABLE dept_emp (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL
+);
+
+--verify table dept_emp was created successfully
+SELECT * FROM dept_emp
+
+
+CREATE TABLE dept_manager (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+--verify table dept_manager was created successfully
+SELECT * FROM dept_manager
+
+
+CREATE TABLE employees (
+    "emp_no" INT   NOT NULL,
+    "emp_title" VARCHAR   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+--verify table employees was created successfully
+SELECT * FROM employees
+
+
+CREATE TABLE salaries (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+--verify table salaries was created successfully
+SELECT * FROM salaries
+
+
+CREATE TABLE titles (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+--verify table titles was created successfully
+SELECT * FROM titles
+
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
 
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 --JOIN SALARIES AND EMPLOYEES TABLES
 
+
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date between '1/1/1986' and '12/31/1986';
+
+
+
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
---SELECT first_name, last_name, sex
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 --G
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
